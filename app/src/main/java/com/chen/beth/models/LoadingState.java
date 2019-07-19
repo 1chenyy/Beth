85f7f6517102774c93b751056e009a6cd2b83937
chenyang <chenyang4346@foxmail.com>
2019-07-19 16:53:19 +0800

    delete

--- app/src/main/java/com/chen/beth/models/LoadingState.java
+++
@@ -1,5 +0,0 @@
-package com.chen.beth.models;
-
-public enum LoadingState {
-    LODING,LOADING_SUCCEED,LOADING_FAILED
-}
--- app/src/main/java/com/chen/beth/models/NetState.java
+++
@@ -1,5 +0,0 @@
-package com.chen.beth.models;
-
-public enum NetState {
-    CONNECTED,DISCONNECTED,NONE
-}
